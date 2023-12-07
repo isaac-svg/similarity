5d115098050b49629293e7c0d983ed6036f3e713
isaac-svg <isaacsakyi009@gmail.com>
2023-12-07 10:02:04 +0000

    session-error

--- app/(docs)/documentation/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-const Layout = ({children}:{children:ReactNode}) => {
-  return (
-    <section className='pt-20'>{children}</section>
-  )
-}
-
-export default Layout
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,76 @@
+import { db } from '@/lib/db'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google'
+import twitterProvider from 'next-auth/providers/twitter'
+
+
+function getGoogleCredentials(): { clientId: string; clientSecret: string } {
+  const clientId = process.env.GOOGLE_CLIENT_ID
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+  if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_ID')
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_SECRET')
+  }
+
+  return { clientId, clientSecret }
+}
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    twitterProvider({
+      clientId:process.env.TWITTER_ID as string,
+      clientSecret:process.env.TWITTER_SECRET as string,
+      version:"2.0"
+    })
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        token.id = user!.id
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+    redirect() {
+      return '/dashboard'
+    },
+  },
+}
+
+
+export default NextAuth(authOptions)
