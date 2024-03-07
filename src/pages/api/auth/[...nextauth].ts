f384d2e354c71957594ceca573495f83547e704f
isaac-svg <isaacsakyi009@gmail.com>
2024-03-07 14:26:27 -0500

    done

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -30,11 +30,7 @@ export const authOptions: NextAuthOptions = {
     signIn: '/login',
   },
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
-      version:"2.0"
-    }),
+    GoogleProvider(getGoogleCredentials()),
     // twitterProvider({
     //   clientId:process.env.TWITTER_ID as string,
     //   clientSecret:process.env.TWITTER_SECRET as string,
